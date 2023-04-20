eadd01b170a6b82c4abbc7d1a63fcb12668fe6fb
pnaik <pnaik@redhat.com>
2023-04-20 11:15:31 +0530

    chore: Added patternfly components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import "./App.css";
 import "@patternfly/react-core/dist/styles/base.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import {
+  Accordion,
+  Breadcrumb,
+  BreadcrumbItem,
+  AccordionItem,
+  AccordionContent,
+  AccordionToggle,
+} from "@patternfly/react-core";
 import Counter from "./components/Counter";
+import { Forms } from "./components/Forms";
+import Home from "./components/Home";
 function App() {
-  return <Counter initialCount={0} />;
+  return (
+    <>
+      <BrowserRouter>
+        <Breadcrumb>
+          <BreadcrumbItem to="/">Section home</BreadcrumbItem>
+          <BreadcrumbItem to="/Counter">Form</BreadcrumbItem>
+          <BreadcrumbItem to="#">Section title</BreadcrumbItem>
+          <BreadcrumbItem to="#" isActive>
+            Section landing
+          </BreadcrumbItem>
+        </Breadcrumb>
+        <Routes>
+          <Route path="/" element={<Counter initialCount={0} />} />
+          <Route path="/Counter" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 export default App;
